3d301f1962e138261bc907456ab8934d7ecda61a
Yuriy Pankiv <pankiv97@gmail.com>
2018-02-13 20:27:35 +0200

    Added default attack debouncer

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let card2 = document.getElementById("card2");
 let minion1 = Object.create(Swordmaster);
 let minion2 = Object.create(Imp);
 
+let minion1Debouncer = true;
+let minion2Debouncer = true;
+
 prepareMinionToFight(minion1);
 prepareMinionToFight(minion2);
 
@@ -11,17 +14,25 @@ displayMinion(card1, minion1);
 displayMinion(card2, minion2);
 
 card1.addEventListener('click', () => {
-	hit(minion1, minion2);
-	cardHitAnimation(document.querySelector('#card1 > .card'), 'move-right', animationOffset);
-	displayMinion(card1, minion1);
-	displayMinion(card2, minion2, true);
-	console.log(minion1, minion2);
+	if (minion2.isAlive && minion1.isAlive && minion1Debouncer) {
+		minion1Debouncer = false;
+		setTimeout(()=>{minion1Debouncer = true}, 1100);
+		hit(minion1, minion2);
+		cardHitAnimation(document.querySelector('#card1 > .card'), 'move-right', animationOffset);
+		displayMinion(card1, minion1);
+		displayMinion(card2, minion2, true);
+		console.log(minion1, minion2);
+	}
 })
 
 card2.addEventListener('click', () => {
-	hit(minion2, minion1);
-	cardHitAnimation(document.querySelector('#card2 > .card'), 'move-left', animationOffset)
-	displayMinion(card2, minion2);
-	displayMinion(card1, minion1, true);
-	console.log(minion1, minion2);
+	if (minion1.isAlive && minion2.isAlive && minion2Debouncer) {
+		minion2Debouncer = false;
+		setTimeout(()=>{minion2Debouncer = true}, 1100);
+		hit(minion2, minion1);
+		cardHitAnimation(document.querySelector('#card2 > .card'), 'move-left', animationOffset)
+		displayMinion(card2, minion2);
+		displayMinion(card1, minion1, true);
+		console.log(minion1, minion2);
+	}
 })
