705caa630950a9a68adc57b74e9d8291a141b10f
Yuriy Pankiv <pankiv97@gmail.com>
2018-02-20 19:30:02 +0200

    Added minion selection

--- script/script.js
+++ script/script.js
@@ -1,7 +1,22 @@
+function prepareFight() {
+	prepareMinionToFight(minion1);
+	prepareMinionToFight(minion2);
+
+	displayMinionStaticData(card1, minion1);
+	displayMinionStaticData(card2, minion2);
+	displayMinion(card1, minion1);
+	displayMinion(card2, minion2);	
+}
+
 function fight(minion1, minion2) {
 	setTimeout( () => { card1.click(); }, 0);
 	setTimeout( () => { card2.click(); }, 2400);
 	setTimeout( () => { minion1.isAlive && minion2.isAlive ? fight(minion1, minion2) : null }, 5000);
+}
+
+function selectFighter(select, index) {
+	select.selectedIndex = index;
+	return Object.create(minions[index]);
 }
 
 let card1 = document.getElementById("card1");
@@ -9,6 +24,9 @@ let card2 = document.getElementById("card2");
 
 let card1select = card1.querySelector('select');
 let card2select = card2.querySelector('select');
+
+let card1button = card1.querySelector('button');
+let card2button = card2.querySelector('button');
 
 let minions = [Swordmaster, Gog, Priest, Wolfraider]
 
@@ -23,24 +41,24 @@ minions.forEach( minion => {
 	card2select.add(option2);
 })
 
-let minion1 = Object.create(minions[Math.floor(Math.random()*minions.length)]);
-let minion2 = Object.create(minions[Math.floor(Math.random()*minions.length)]);
+
+let minion1;
+let minion2;
+
+minion1 = selectFighter(card1select, Math.floor(Math.random()*minions.length));
+minion2 = selectFighter(card2select, Math.floor(Math.random()*minions.length));
+
+console.log(minion1);
 
 let minion1Debouncer = true;
 let minion2Debouncer = true;
 
-prepareMinionToFight(minion1);
-prepareMinionToFight(minion2);
-
-displayMinionStaticData(card1, minion1);
-displayMinionStaticData(card2, minion2);
-displayMinion(card1, minion1);
-displayMinion(card2, minion2);
+prepareFight();
 
 applyEffect(0, minion1, minion2);
 applyEffect(0, minion2, minion1);
 
-card1.addEventListener('click', () => {
+card1.querySelector('.card').addEventListener('click', () => {
 	if (minion2.isAlive && minion1.isAlive && minion1Debouncer) {
 		let queue = animationQueue();
 	
@@ -61,7 +79,7 @@ card1.addEventListener('click', () => {
 	}
 })
 
-card2.addEventListener('click', () => {
+card2.querySelector('.card').addEventListener('click', () => {
 	if (minion1.isAlive && minion2.isAlive && minion2Debouncer) {
 		let queue = animationQueue();
 
@@ -84,4 +102,14 @@ card2.addEventListener('click', () => {
 
 document.getElementById('auto-fight').addEventListener('click', () => {
 	fight(minion1, minion2);
+})
+
+card1button.addEventListener('click', () => {
+	minion1 = selectFighter(card1select, card1select.selectedIndex);
+	prepareFight();
+})
+
+card2button.addEventListener('click', () => {
+	minion2 = selectFighter(card1select, card1select.selectedIndex);
+	prepareFight();
 })
